dab34cbf6032c498a0d3dc2292ec6b336efdec87
Rosine <tuyishimirer1@gamil.com>
2026-09-04 15:46:25 +0200

    Trigger backend redeploy - Properties module initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,6 +32,7 @@ import { AppService } from './app.service';
     PrismaModule,
     CloudinaryModule,
     AuthModule,
+    PropertiesModule, // Properties marketplace
     ProjectsModule,
     ProjectImagesModule,
     ProjectAssetsModule,
@@ -48,7 +49,7 @@ import { AppService } from './app.service';
     CareersModule,
     ApplicationsModule,
     PaymentsModule,
-    PropertiesModule,
+    PropertiesModule, // Real estate marketplace
     SearchModule,
     DashboardModule,
     SettingsModule,
